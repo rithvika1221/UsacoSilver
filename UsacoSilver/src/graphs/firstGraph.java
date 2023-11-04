41f2eb43eead4173e74e455826262033b9f28427
Rithvika Devisetti <70250513+rithvika1221@users.noreply.github.com>
2023-11-03 20:31:52 -0700

    graphs

--- UsacoSilver/src/graphs/firstGraph.java
+++ UsacoSilver/src/graphs/firstGraph.java
@@ -8,12 +8,17 @@ import java.util.ArrayList;
 public class firstGraph {
 
 	public static void main(String[] args) throws IOException {
-		graph g= new graph(5);
-		g.addEdge(0, 1);
-		g.addEdge(0, 2);
+		graph g= new graph(7);
 		g.addEdge(1, 2);
+        g.addEdge(1, 3 );
+        g.addEdge(2, 4);
+        g.addEdge(3, 5);
+        g.addEdge(4, 6);
+        g.addEdge(5, 6);
+       
 		
-		g.print();
+		g.dfs(1);
+		
 		
 	}
 
--- UsacoSilver/src/graphs/graph.java
+++ UsacoSilver/src/graphs/graph.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class graph {
 
-	ArrayList<ArrayList<Integer>> graph ;
+	ArrayList<ArrayList<Integer>> graph = new 	ArrayList<ArrayList<Integer>>();
 	int V;
 
 	public graph(int nodes) {
@@ -22,6 +22,62 @@ public class graph {
 		graph.get(v).add(u);
 		graph.get(u).add(v);
 
+	}
+	
+	public void dfs(int i)
+	{
+		HashSet<Integer> visited = new HashSet();
+		Stack<Integer> stack = new Stack();
+		stack.push(i);
+		
+		
+		while(!stack.isEmpty())
+		{
+			int v = stack.pop();
+			if (!visited.contains(v))
+			{
+				visited.add(v);
+				System.out.println(v);
+			}
+			
+			for(int j: graph.get(v)){
+				if(!visited.contains(j))
+				{
+					stack.push(j);
+				}
+				
+			}
+			
+			
+		}
+		
+		
+	}
+	
+	
+	public void bfs(int i)
+	{
+		HashSet<Integer> visited2 = new HashSet();
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(i);
+				
+		while(!queue.isEmpty())
+		{
+			int vi = queue.poll();
+			if (!visited2.contains(vi))
+			{
+				visited2.add(vi);
+				System.out.println(vi);
+			}
+			
+			for(int j: graph.get(vi))
+			{
+				queue.add(j);
+				
+			}
+				
+		}
+		
 	}
 
 	public void print() {
